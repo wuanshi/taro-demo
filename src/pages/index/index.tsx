745b110520d86007bef99ec9a70607434fd24b12
liliang <1497809786@qq.com>
2024-01-10 21:46:25 +0800

    fix: 🐛 test fix version
    BREAKING CHANGE: 🧨 2.1.2

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,28 +1,56 @@
-import { useRef } from 'react'
-import { View, Button } from '@tarojs/components'
-import { useLoad } from '@tarojs/taro'
-import Dropdown from '@/components/Dropdown'
-import './index.scss'
+import { useEffect, useRef } from "react";
+import { View, Canvas } from "@tarojs/components";
+import Taro, { useLoad } from "@tarojs/taro";
+import lottie from "lottie-miniprogram";
+// import lottieAniamte from "./lottie-animate.json";
+import lottieAniamte from "./heart-icon.json"
+// import Dropdown from "@/components/Dropdown";
+import "./index.scss";
 
 export default function Index() {
-  const ref = useRef<any>()
+  const ref = useRef<any>();
   useLoad(() => {
-    console.log('Page loaded.')
-  })
+    console.log("Page loaded.");
+  });
+
+  useEffect(() => {
+    const query = Taro.createSelectorQuery();
+    query
+      .select("#EXISAHFSFSDSDFHI")
+      .node((res) => {
+        console.log("res", res);
+        setTimeout(() => {
+          const canvas = res.node;
+          // canvas.width = 300
+          // canvas.height = 300
+          const context = canvas.getContext("2d");
+          lottie.setup(canvas);
+          ref.current = lottie.loadAnimation({
+            loop: false,
+            autoplay: false,
+            animationData: lottieAniamte,
+            // path: "https://lottie.host/b7533b80-b243-42eb-9274-c3550e019893/NTrZ9BFZEA.json", // 动画文件路径
+            rendererSettings: {
+              context,
+            },
+          });
+        }, 300)
+
+      })
+      .exec();
+  }, []);
 
   return (
     <View className='index'>
-      <Dropdown ref={ref}>
-        <Dropdown.Item title='标题1' key={1}>
-          <View>111</View>
-        </Dropdown.Item>
-        <Dropdown.Item title='标题2' key={2}>
-          <>
-            <View>222</View>
-            <Button onClick={() => ref.current.close()}>close dropdown</Button>
-          </>
-        </Dropdown.Item>
-      </Dropdown>
+      <Canvas
+        style={{ width: '100%', height: '100%' }}
+        type='2d'
+        canvasId='EXISAHFSFSDSDFHI'
+        id='EXISAHFSFSDSDFHI'
+        onClick={() => {
+          ref.current.goToAndPlay(0, true);
+        }}
+      />
     </View>
-  )
+  );
 }
---
+++ src/pages/richtext/index.tsx
@@ -0,0 +1,59 @@
+import Taro from "@tarojs/taro";
+import { View } from "@tarojs/components";
+import "@tarojs/taro/html5.css";
+import "./index.scss";
+
+const TableEl = `<table><colgroup><col style="width: 38.4901%;" width="348" /><col width="243" /><col /><col /></colgroup>
+<tbody>
+<tr>
+<td data-sheet-value="&quot;项目名称&quot;">项目名称</td>
+<td data-sheet-value="&quot;适合方向&quot;">适合方向</td>
+<td height="27" data-sheet-value="&quot;申请要求&quot;">申请要求</td>
+<td>官网网址</td>
+</tr>
+<tr>
+<td data-sheet-value="&quot;美国-明尼苏达大学Carlson商学院&quot;">美国-明尼苏达大学Carlson商学院</td>
+<td rowspan="5" width="243" data-sheet-value="&quot;不限&quot;">不限</td>
+<td rowspan="5" width="461" data-sheet-value="&quot;1.学业成绩优良，所有课程平均学积分不得低于75分，在校期间课程无不及格记录（通识选修课、个性化课程、二专课程除外；考察成绩范围：入学&mdash;2022秋季学期）；\n2.具有良好的外语水平，具体成绩要求为：CET-4&ge;500分、TOFEL&ge;90分、IELTS&ge;6.0分，满足任一条件即可；对于有特别要求的学校，以交换学校的外语要求为准；&quot;">
+<p>1.学业成绩优良，所有课程平均学积分不得低于75分，在校期间课程无不及格记录（通识选修课、个性化课程、二专课程除外；考察成绩范围：入学&mdash;2022秋季学期）；</p>
+<p>1.学业成绩优良，所有课程平均学积分不得低于75分，在校期间课程无不及格记录（通识选修课、个性化课程、二专课程除外；考察成绩范围：入学&mdash;2022秋季学期）；<br />2.具有良好的外语水平，具体成绩要求为：CET-4&ge;500分、TOFEL&ge;90分、IELTS&ge;6.0分，满足任一条件即可；对于有特别要求的学校，以交换学校的外语要求为准；</p>
+</td>
+<td rowspan="5">
+<p><a href="https://www.compassedu.com" target="_blank" rel="noopener">查看官网报名链接</a></p>
+</td>
+</tr>
+<tr >
+<td  data-sheet-value="&quot;加拿大-麦吉尔大学Desautels管理学院&quot;">加拿大-麦吉尔大学Desautels管理学院</td>
+</tr>
+<tr >
+<td  data-sheet-value="&quot;英国-利兹大学商学院&quot;">英国-利兹大学商学院</td>
+</tr>
+<tr >
+<td data-sheet-value="&quot;新加坡-南洋理工大学南洋商学院&quot;">新加坡-南洋理工大学南洋商学院</td>
+</tr>
+<tr >
+<td  data-sheet-value="&quot;澳大利亚-悉尼大学&quot;">澳大利亚-悉尼大学</td>
+</tr>
+</tbody>
+</table>
+`;
+
+function RichText() {
+
+  return (
+    <View className="taro_html" dangerouslySetInnerHTML={{ __html: TableEl }}>
+      {(Taro.options.html.transformElement = (el) => {
+        if (el.nodeName === 'image') {
+          el.setAttribute('mode', 'widthFix')
+          el.__handlers.tap = [
+            () => {
+            },
+          ]
+        }
+        return el
+      })}
+    </View>
+  );
+}
+
+export default RichText;
